893fa12659c682e96ba7b214bfaa4a8a72452a50
胥明胜 <1362624542@qq.com>
2017-10-31 17:13:41 +0800

    10.31.17时13分

--- WeShop.Web/Controllers/HomeController.cs
+++ WeShop.Web/Controllers/HomeController.cs
@@ -15,15 +15,16 @@ namespace WeShop.Web.Controllers
         public IBannerService BannerService { get; set; }
         public IProductService ProductService { get; set; }
         public  INoticeService NoticeService { get; set; }
-        public ActionResult Index(string type)
+        public ActionResult Index(string type="1")
         {
             int? CartProNum = null;
             ViewBag.CartProNum = CartProNum;
+
             HomeViewModel homeViewModel = new HomeViewModel();
             homeViewModel.Banners = BannerService.GetEntities(b => true);
             homeViewModel.NoticeNum = NoticeService.GetCount(n => true);
             homeViewModel.Notices = NoticeService.GetEntitiesByPage(3, 1, false, n => true, n => n.ModiTime);
-            homeViewModel.Products = ProductService.GetEntitiesByPage(3, 1, false, p => p.Type==1, p => p.ModiTime);
+            homeViewModel.Products = ProductService.GetEntitiesByPage(3, 1, false, p => p.Type.ToString()==type, p => p.ModiTime);
 
             homeViewModel.UserInfo=Session["userinfo"] as OAuthUserInfo;
             return View(homeViewModel);
